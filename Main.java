6f7be5ea25aebd29e4053d50db6cd0116a258a0b
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-22 22:05:19 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package days_14;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int i = 0;
+while (i < 5) {
+System.out.println(i);
+i++;
+    
+    }
+    }
+}
